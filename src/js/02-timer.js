31c066dfcd955155339d8717094ce27fddd80968
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-11-23 19:01:53 +0100

    Adding files

---
+++ src/js/02-timer.js
@@ -0,0 +1,18 @@
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+function nameString(name) {
+  var b = 'Sucks';
+  var result = name + '' + b;
+  return result;
+}
+console.log(nameString('Max'));
